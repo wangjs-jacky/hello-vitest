ba68fa3ee4834f89f1028123f114c83581547125
所无聊 <2409277719@qq.com>
2023-03-14 21:53:36 +0800

    💥 feat: 对 window.storage 函数进行测试,以及环境搭建

---
+++ README.md
@@ -0,0 +1,5 @@
+## 指令说明
+
+- `npm run test:init` ： 执行测试。
+- `npm run test:ui`：开启可视化界面
+- `npm run test:coverage`：生成测试覆盖率报告
--- src/utils/__test__/index.test.ts
+++ src/utils/__test__/index.test.ts
@@ -1,8 +1,21 @@
-import { expect, test } from "vitest";
+import { expect, test, describe, it } from "vitest";
+import storage from "../storage";
 import { sum } from "../sum";
 
 test("add", () => {
   expect(sum(1, 1)).toBe(2);
   expect(sum(2, 3)).toMatchSnapshot();
-  expect(sum(4, 5)).toMatchInlineSnapshot('9');
+  expect(sum(4, 5)).toMatchInlineSnapshot("9");
+});
+
+describe("storage", () => {
+  it("可以缓存值", () => {
+    storage.set("newKey", "hello");
+    expect(localStorage.getItem("my-app-newKey")).toEqual("hello");
+  });
+
+  it("可以设置值", () => {
+    localStorage.setItem("my-app-newKey", "hello");
+    expect(storage.get("newKey")).toEqual("hello");
+  });
 });
